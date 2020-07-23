f7cc0fc04229fbf9e3d84647591abe7cd59e1bcf
Neo <liveabean@163.com>
2020-07-23 23:42:26 +0800

    feat(util): deep-object-diff

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -60,7 +60,6 @@ export function map(obj, func) {
 
 /**
  * TODO: Zero dependency
- * More: https://github.com/mattphillips/deep-object-diff/blob/master/src/utils/index.js
  * Ref: https://gist.github.com/Yimiprod/7ee176597fef230d1451
  * Deep diff between two object, using lodash
  * @param  {Object} object Object compared
@@ -78,6 +77,40 @@ export function difference(object, base) {
     const result = changes(object, base)
     return result;
 }
+
+/**
+ * * More: https://github.com/mattphillips/deep-object-diff/blob/master/src/utils/index.js
+ */
+const properObject = o => isObject(o) && !o.hasOwnProperty ? { ...o } : o;
+const isDate = d => d instanceof Date;
+const isEmpty = o => Object.keys(o).length === 0;
+const diff = (lhs, rhs) => {
+  if (lhs === rhs) return {}; // equal return no diff
+
+  if (!isObject(lhs) || !isObject(rhs)) return rhs; // return updated rhs
+
+  const l = properObject(lhs);
+  const r = properObject(rhs);
+
+  const deletedValues = Object.keys(l).reduce((acc, key) => {
+    return r.hasOwnProperty(key) ? acc : { ...acc, [key]: undefined };
+  }, {});
+
+  if (isDate(l) || isDate(r)) {
+    if (l.valueOf() == r.valueOf()) return {};
+    return r;
+  }
+
+  return Object.keys(r).reduce((acc, key) => {
+    if (!l.hasOwnProperty(key)) return { ...acc, [key]: r[key] }; // return added r key
+
+    const difference = diff(l[key], r[key]);
+
+    if (isObject(difference) && isEmpty(difference) && !isDate(difference)) return acc; // return no diff
+
+    return { ...acc, [key]: difference }; // return updated key
+  }, deletedValues);
+};
 
 export function debounce(func, wait) {
     let timeout;
